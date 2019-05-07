47f6dd57b0381b877e6f7f933a13e9e602a849aa
neruneru0419 <neruneru55555@gmail.com>
2019-05-07 15:12:08 +0900

    おみくじ機能を追加

--- get.rb
+++ get.rb
@@ -1,6 +1,6 @@
 require 'http'
 require 'json'
-require "./get"
+require "./command"
 def get_ts
     response = HTTP.post("https://slack.com/api/channels.history", params: {
         token: ENV['SLACK_API_TOKEN'],
