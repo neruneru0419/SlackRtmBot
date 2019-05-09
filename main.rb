8fd3633588266b0333bc589c843347521017c975
Nerun <39396637+neruneru0419@users.noreply.github.com>
2019-05-09 23:48:00 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
+require 'faye/websocket'
 require 'aws-sdk-v1'
 require 'http'
 require 'json'
 require 'eventmachine'
-require 'faye/websocket'
-require './unknown'
-
+require "./unknown"
+require "./girak"
 response = HTTP.post("https://slack.com/api/rtm.start", params: {
     token: ENV['SLACK_API_TOKEN']
   })
@@ -14,11 +14,20 @@ $username = $girak_value["username"]
 rc = JSON.parse(response.body)
 
 url = rc['url']
+ary = []
+ary += girak_text("C8DCH0KA6")
+ary += girak_text("CAT2HJBQX") 
+ary += girak_text("CJHUGT97W") 
+ary += girak_text("CF51F0YFR") 
+ary += girak_text("CEABD696C")
+ary += girak_text("CBMJ0BMT6")
+ary += girak_text("CAZ7HSFU6")
 
 EM.run do
   # Web Socketインスタンスの立ち上げ
   ws = Faye::WebSocket::Client.new(url)
   flg = true
+  flg2 = true
 
   #  接続が確立した時の処理
   ws.on :open do
@@ -28,13 +37,22 @@ EM.run do
   ws.on :message do |event|
     data = JSON.parse(event.data)
     p [:message, data]
-
-    if data['text'].is_a?(String) and data['channel'] == 'CFG3HU6TA' then
-      if flg then
-        unknown()
-        flg = false
-      else 
-        flg = true
+    
+    if data['text'].is_a?(String) then
+      if data['channel'] == 'CFG3HU6TA' then
+        if flg then
+          unknown()
+          flg = false
+        else 
+          flg = true
+        end
+      elsif data['channel'] == 'CJHUGT97W' then
+        if flg then
+          girak_learn(ary)
+          flg2 = false
+        else 
+          flg2 = true
+        end
       end
     end
   end
